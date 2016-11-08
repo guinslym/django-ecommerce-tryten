eee1aaf1bee1f230e55d9e0997360dd5e592aa9b
Guinslym <guinslym@gmail.com>
2016-11-07 22:53:16 -0500

    staticfiles part 2

--- tryten/settings.py
+++ tryten/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if DEBUG:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join('static', 'static')
     MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'static-only')
-    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'media')
-    STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'static', 'static')
-)
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'media')
+    STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
