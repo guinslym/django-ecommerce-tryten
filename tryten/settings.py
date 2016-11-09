5f85ff1303b8fcfd77b14b9f7390156be6647ac6
Guinsly Mondésir <guinslym@gmail.com>
2016-11-09 03:02:56 -0500

    staticfiles

--- tryten/settings.py
+++ tryten/settings.py
@@ -126,5 +126,5 @@ if DEBUG:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'media')
     STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
     )
